1da501f12ecd313e557dbb1efbe48be98b386e79
hahahellooo <hahahello777@gmail.com>
2024-10-25 14:20:10 +0900

    stop log

--- sv.sh
+++ sv.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-DATE=$(date +"%Y%m%d%H%M%S")
-cat ~/.zsh_history | cut -d';' -f2- | sort | uniq > history_$DATE.log
+#DATE=$(date +"%Y%m%d%H%M%S")
+#cat ~/.zsh_history | cut -d';' -f2- | sort | uniq > history_$DATE.log
 
